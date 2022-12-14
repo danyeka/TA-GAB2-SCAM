2de4a23d5ac100e6a87bf7f08ec6198ac7faf467
PratamaBudiRusadi <Pratamabudi455@gmail.com>
2022-12-14 16:04:11 +0700

    pratama: menambahkan function post

--- Server/application/controllers/Pegawai.php
+++ Server/application/controllers/Pegawai.php
@@ -10,21 +10,50 @@ class Pegawai extends Server {
     public function __construct()
         {
                 parent::__construct();
-                 //panggil model "Mmahasiswa"
+                 //panggil model "Ppegawai"
                 $this->load->model("Ppegawai","model",TRUE);
         }
 
 	//buat fungsi "GET"
     function service_get()
     {
-       
-        
         //panggil fungsi "get_data"
        $hasil = $this->model->get_data();
 
         $this->response(array("pegawai" =>
         $hasil),200);
-
     }
+    //buat fungsi "POST"
+    function service_post()
+    {
+       
+        //ambil parameter token data yang akan diisi
+        $data = array(
+            "nik" => $this->post("nik"),
+            "nama_pegawai" => $this->post("nama_pegawai"),
+            "jenis_kelamin" => $this->post("jenis_kelamin"),
+            "jabatan" => $this->post("jabatan"),
+            "tanggal_masuk" => $this->post("tanggal_masuk"),
+            "status" => $this->post("status"),
+            "photo" => $this->post("photo"),
+            "token" => base64_encode($this->post("token")),
+        );
+        // panggil method "save data"
+        $hasil = $this->model->save_data($data["nik"],
+        $data["nama_pegawai"],$data["jenis_kelamin"],$data["jabatan"],$data["tanggal_masuk"],$data["status"],
+        $data["photo"],$data["token"]);
+        // jika hasil = 0
+        if($hasil == 0 )
+        {
+            $this->response(array("status" =>"Data Pegawai Berhasil Disimpan"),200);
+        }
+        // jika hasil != 0
+        else
+        {
+            $this->response(array("status" =>"Data Pegawai  Gagal Disimpan !"),200);
+        }
 
+
+
+}
 }
--- Server/application/models/Ppegawai.php
+++ Server/application/models/Ppegawai.php
@@ -6,8 +6,8 @@ class Ppegawai extends CI_Model {
 	// buat method untuk tampil data
     function get_data()
     {
-        //mengambil hanya npm
-        //$this->db->select("npm")
+        //mengambil hanya nik
+        //$this->db->select("nik")
 
         $this->db->select("id_pegawai AS id_pgw,
         nik AS nik_pgw,
@@ -26,7 +26,43 @@ class Ppegawai extends CI_Model {
         return $query;
 
     }
+    
+    // buat fungsi untuk simpan data
+    function save_data($nik,$nama_pegawai,$jenis_kelamin,$jabatan,$tanggal_masuk,$status,$photo,$token)
+    {
+       // cek apakah nik ada/tidak
+       $this->db->select("nik");
+       $this->db->from("data_pegawai");
+       $this->db->where("TO_BASE64(nik) = '$token'");
+       //eksekusi query
+       $query = $this->db->get()->result();
+       //jika nik ditemukan
+       if(count($query) == 0)
+       {
+         // isi nilai untuk masing2 field
+         $data = array(
+            "nik" => $nik,
+            "nama_pegawai" => $nama_pegawai,
+            "jenis_kelamin" => $jenis_kelamin,
+            "jabatan" => $jabatan,
+            "tanggal_masuk" => $tanggal_masuk,
+            "status" => $status,
+            "photo" => $photo,
+         );
+         // simpan data
+         $this->db->insert("data_pegawai",$data);
+         $hasil= 0;
 
+       }
+      // jika nik ditemukan
+      else
+      {
+        $hasil = 1;
+      }
+      return $hasil;
+    }
 
+    
+    
 }
 
