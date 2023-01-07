581d094d100add08c28dae8dea3675939f0a1063
PratamaBudiRusadi <pratamabudi455#gmail.com>
2023-01-07 06:49:15 -0800

    pratama : menambahkan controller DataPegawai.php

---
+++ Client/application/controllers/admin/DataPegawai.php
@@ -0,0 +1,10 @@
+<?php
+//masih error
+class dataPegawai extends CI_Controller{
+    public function index()
+    {
+        $data = $this->db->query("SELECT * FROM data_pegawai")->result();
+        var_dump($data);
+    }
+}
+?>
