967566dc45186b076dd26a31f47f410641768bce
Pratama Budi <pratamabudi455@gmail.com>
2023-01-25 12:11:52 +0700

    pratama : membuat function index di controller dashboard untuk menampilan jumlah jabatan, kehadiran, pegawai, dan admin

--- Client/application/controllers/admin/Dashboard.php
+++ Client/application/controllers/admin/Dashboard.php
@@ -19,14 +19,21 @@ class Dashboard extends CI_Controller{
     public function index()
     {
         $data['title'] = "Dashboard";
+
+        $jabatan = json_decode($this->client->simple_get(API_DATA_JABATAN));
+        $kehadiran = json_decode($this->client->simple_get(API_DATA_ABSEN));
+        $pegawai = json_decode($this->client->simple_get(API_DATA_PEGAWAI));
+
+        $data['admin'] = $pegawai->admin_jumlah;
+        $data['pegawai'] = $pegawai->pegawai_jumlah;
+        $data['jabatan'] = $jabatan->jabatan_jumlah;
+        $data['kehadiran'] = $kehadiran->kehadiran_jumlah;
+
         $this->load->view('templates_admin/header',$data);
         $this->load->view('templates_admin/sidebar');
         $this->load->view('admin/dashboard',$data);
-
         $this->load->view('templates_admin/footer');
-
     }
-
 }
 
 ?>
