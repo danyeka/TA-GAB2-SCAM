de3f23d8ba1951cf40801f1f37d294a40e5252f7
Pratama Budi <pratamabudi455@gmail.com>
2023-01-25 12:57:00 +0700

    prataman : membuat function tambah data di controller data pegawai untuk menampilkan tambah data pegawai

--- Client/application/controllers/admin/DataPegawai.php
+++ Client/application/controllers/admin/DataPegawai.php
@@ -28,6 +28,17 @@ class DataPegawai extends CI_Controller{
         $this->load->view('admin/data_pegawai',$data);
         $this->load->view('templates_admin/footer');
     }
+
+    public function tambah_data()
+    {
+        $send = array('id' => "");
+        $data['title'] = "Tambah Data Pegawai";
+        $data['jabatan'] = json_decode($this->client->simple_get(API_DATA_JABATAN, $send))->jabatan;
+        $this->load->view('templates_admin/header',$data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/tambah_pegawai',$data);
+        $this->load->view('templates_admin/footer');
+    }
     }
 }
 
