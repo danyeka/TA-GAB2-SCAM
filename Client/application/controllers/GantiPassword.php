d1b6719516814a6eebeead26da55cdcbae3f92f2
Pratama Budi <pratamabudi455@gmail.com>
2023-01-25 12:54:10 +0700

    prataman : membuat function index di controller ganti password untuk update password berdasarkan id

--- Client/application/controllers/GantiPassword.php
+++ Client/application/controllers/GantiPassword.php
@@ -2,6 +2,14 @@
 
 class GantiPassword extends CI_Controller{
     
+    public function index()
+    {
+        $data['title'] = "Ganti Password";
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('ganti_password', $data);
+        $this->load->view('templates_admin/footer');
+    }
 }
 
 ?>
