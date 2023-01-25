47cf5f4f6db53c996fa52d60a7891d7842912e59
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:23:56 +0700

    rifan : membuat function service post controller data pegawai update untuk mengupdate data pegawai

--- Server/application/controllers/DataPegawaiUpdate.php
+++ Server/application/controllers/DataPegawaiUpdate.php
@@ -21,4 +21,34 @@ class DataPegawaiUpdate extends Server {
             $this->response(array("pesan" => "false"),200);
         }
     }
+
+    function service_post()
+    {
+        if (empty($this->post("photo"))){
+            $data = array(
+            "nik" => $this->post("nik"),
+            "nama_pegawai" => $this->post("nama_pegawai"),
+            "jenis_kelamin" => $this->post("jenis_kelamin"),
+            "jabatan" => $this->post("jabatan"),
+            "tanggal_masuk" => $this->post("tanggal_masuk"),
+            "hak_akses" => $this->post("hak_akses"),
+            "status" => $this->post("status")
+            );
+        } else {
+            $data = array(
+            "nik" => $this->post("nik"),
+            "nama_pegawai" => $this->post("nama_pegawai"),
+            "jenis_kelamin" => $this->post("jenis_kelamin"),
+            "jabatan" => $this->post("jabatan"),
+            "tanggal_masuk" => $this->post("tanggal_masuk"),
+            "status" => $this->post("status"),
+            "hak_akses" => $this->post("hak_akses"),
+            "photo" => $this->post("photo")
+            );
+        }
+
+        $hasil = $this->model->update_data($data, $this->post("id_pegawai"));
+        if($hasil == 0 ) $this->response(array("pesan" => "true"),200);
+        else $this->response(array("pesan" => "false"),200);
+    }
 }
