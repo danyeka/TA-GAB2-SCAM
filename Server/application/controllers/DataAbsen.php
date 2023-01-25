a14f92b6e9bac51b1b19e939c79b306ede70327a
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:05:52 +0700

    rifan : membuat function __construct di model absen untuk load model absen

--- Server/application/controllers/DataAbsen.php
+++ Server/application/controllers/DataAbsen.php
@@ -5,4 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class DataAbsen extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Mabsen","model",TRUE);
+    }
 }
