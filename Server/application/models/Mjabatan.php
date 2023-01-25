862b511c4a37a98ba200874977eabec0aaad4ffa
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 10:57:29 +0700

    rifan : membuat function get_data di model jabatan

--- Server/application/models/Mjabatan.php
+++ Server/application/models/Mjabatan.php
@@ -2,5 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MJabatan extends CI_Model {
+
+    function get_data($id)
+    {
+        $this->db->select("*");
+        $this->db->from("data_jabatan");
+        if(empty($id)) $this->db->order_by("nama_jabatan", "ASC");
+        else $this->db->where("id_jabatan = '$id'");
+        return $this->db->get()->result();
+    }
 }
 
