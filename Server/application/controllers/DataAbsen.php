a36a4f1dc6330b86f39735fc606128dbc99a93fa
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:07:50 +0700

    rifan : membuat function service post di controller absen untuk menyimpan data pegawai

--- Server/application/controllers/DataAbsen.php
+++ Server/application/controllers/DataAbsen.php
@@ -19,4 +19,36 @@ class DataAbsen extends Server {
         $this->response(array("absen" => $hasil, "kehadiran_jumlah" => $number),200);
     }
     
+    function service_post()
+    {
+       
+        //ambil parameter token data yang akan diisi
+        $data = array(
+            "nik" => $this->post("nik"),
+            "nama_pegawai" => $this->post("nama_pegawai"),
+            "jenis_kelamin" => $this->post("jenis_kelamin"),
+            "jabatan" => $this->post("jabatan"),
+            "tanggal_masuk" => $this->post("tanggal_masuk"),
+            "status" => $this->post("status"),
+            "photo" => $this->post("photo"),
+            "token" => base64_encode($this->post("token")),
+        );
+        // panggil method "save data"
+        $hasil = $this->model->save_data($data["nik"],
+        $data["nama_pegawai"],$data["jenis_kelamin"],$data["jabatan"],$data["tanggal_masuk"],$data["status"],
+        $data["photo"],$data["token"]);
+        // jika hasil = 0
+        if($hasil == 0 )
+        {
+            $this->response(array("status" =>"Data Pegawai Berhasil Disimpan"),200);
+        }
+        // jika hasil != 0
+        else
+        {
+            $this->response(array("status" =>"Data Pegawai  Gagal Disimpan !"),200);
+        }
+
+
+
+}
 }
