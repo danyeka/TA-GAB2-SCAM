5d937e32236ec1c84531aec9a61bf7f725f99831
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:16:42 +0700

    rifan : membuat __construct untuk mengambil model potongan gaji di controller potongan gaji update

--- Server/application/controllers/PotonganGajiUpdate.php
+++ Server/application/controllers/PotonganGajiUpdate.php
@@ -5,4 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class PotonganGajiUpdate extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Mpotongan_gaji", "model", TRUE);
+    }
+
 }
