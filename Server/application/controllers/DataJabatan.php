00368d5704992e95edbb5c930237cf83fcae374c
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:14:35 +0700

    rifan : membuat __construct untuk mengambil model jabatan di controller data jabatan

--- Server/application/controllers/DataJabatan.php
+++ Server/application/controllers/DataJabatan.php
@@ -5,4 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class DataJabatan extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Mjabatan", "model", TRUE);
+    }
 }
