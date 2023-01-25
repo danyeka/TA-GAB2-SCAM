cb6421036e6cd051ba166be6c0a3c567a4df263c
Pratama Budi <pratamabudi455@gmail.com>
2023-01-25 12:58:23 +0700

    prataman : membuat function tambah data aksi di controller data pegawai untuk mengecek input tambah data dan menyimpan data yang sudah di validasi ke database

--- Client/application/controllers/admin/DataPegawai.php
+++ Client/application/controllers/admin/DataPegawai.php
@@ -39,6 +39,71 @@ class DataPegawai extends CI_Controller{
         $this->load->view('admin/tambah_pegawai',$data);
         $this->load->view('templates_admin/footer');
     }
+
+    public function tambah_data_aksi()
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE){
+            $this->tambah_data();
+        } else {
+            $nik = $this->input->post('nik');
+            $nama_pegawai = $this->input->post('nama_pegawai');
+            $jenis_kelamin = $this->input->post('jenis_kelamin');
+            $tangga_masuk = $this->input->post('tangga_masuk');
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $jabatan = $this->input->post('jabatan');
+            $status = $this->input->post('status');
+            $hak_akses = $this->input->post('hak_akses');
+            $photo = $_FILES['photo']['name'];
+
+            if($photo= ''){
+
+            } else {
+                $config['upload_path'] = './ext/photo';
+                $config['allowed_types'] = 'jpg|jpeg|png|tiff';
+                $this->load->library('upload', $config);
+                if(!$this->upload->do_upload('photo')){
+                    echo "Photo Gagal diupload!";
+                } else {
+                    $photo = $this->upload->data('file_name');
+                }
+            }
+
+            $data = array(
+                'nik'           => $this->input->post('nik'),
+                'nama_pegawai'  => $this->input->post('nama_pegawai'),
+                'jenis_kelamin' => $this->input->post('jenis_kelamin'),
+                'jabatan'       => $this->input->post('jabatan'),
+                'tanggal_masuk' => $this->input->post('tanggal_masuk'),
+                'status'        => $this->input->post('status'),
+                'hak_akses'     => $this->input->post('hak_akses'),
+                'username'      => $this->input->post('username'),
+                'password'      => $this->input->post('password'),
+                'photo'         => $photo
+            );
+
+            $response = json_decode($this->client->simple_post(API_DATA_PEGAWAI, $data));
+            if ($response->pesan){
+                $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Data berhasil ditambahkan</strong> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            } else {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Data gagal ditambahkan</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            }
+
+            $this->index();
+        }
+    }
     }
 }
 
