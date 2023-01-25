97a34082edfdd21ef2637ecaa67c3f22e306130a
Dany Eka <ekaadany12@gmail.com>
2023-01-25 13:10:33 +0700

    dany : membuat function index di data gaji pegawai yang digunakan untuk memfilter data gaji berdasarkan nik pegawai

--- Client/application/controllers/pegawai/DataGaji.php
+++ Client/application/controllers/pegawai/DataGaji.php
@@ -16,6 +16,18 @@ class DataGaji extends CI_Controller{
         }
     }
     
+    public function index()
+    {
+        $data['title'] = "Data Gaji Pegawai";
+        $gaji = json_decode($this->client->simple_get(API_VALIDASI, array('nik' => $this->session->userdata('nik'))));
+        $potonganGaji = json_decode($this->client->simple_get(API_POTONGAN_GAJI));
+        $data['gaji'] = $gaji->gaji;
+        $data['potongan_gaji'] = $potonganGaji->potongan_gaji[0]->jml_potongan;
+        $this->load->view('templates_admin/header',$data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('pegawai/data_gaji',$data);
+        $this->load->view('templates_admin/footer');
+    }
 }
 
 ?>
