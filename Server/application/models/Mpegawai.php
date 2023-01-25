d60c392bf0d93db457e48235db145d21921144ac
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 10:58:48 +0700

    rifan : membuat function get_data di model pegawai

--- Server/application/models/Mpegawai.php
+++ Server/application/models/Mpegawai.php
@@ -2,5 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mpegawai extends CI_Model {
+
+    function get_data($id)
+    {
+        $this->db->select("*");
+        $this->db->from("data_pegawai");
+        if(empty($id)) $this->db->order_by("nama_pegawai", "ASC");
+        else $this->db->where("id_pegawai = '$id'");
+        return $this->db->get()->result();
+    }
 }
 
