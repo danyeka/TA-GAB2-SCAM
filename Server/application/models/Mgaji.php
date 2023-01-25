848e33d5417f7c0401e19e21331c0ca7d0c7ad68
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 10:57:14 +0700

    rifan : membuat function delete_data di model gaji

--- Server/application/models/Mgaji.php
+++ Server/application/models/Mgaji.php
@@ -20,5 +20,16 @@ class Mgaji extends CI_Model {
       );
       return $this->db->update("potongan_gaji", $data, $where);
     }
+
+    function delete_data($id)
+    {
+      $this->db->where(array('id' => $id));
+      return $this->db->delete("potongan_gaji");
+    }
+
+    function get_data_pegawai ($nik)
+    {
+        return $this->db->query("SELECT data_pegawai.nik, data_pegawai.nama_pegawai, data_jabatan.gaji_pokok, data_jabatan.tj_transport, data_jabatan.uang_makan, data_kehadiran.alpha, data_kehadiran.bulan, data_kehadiran.id_kehadiran FROM data_pegawai INNER JOIN data_kehadiran ON data_kehadiran.nik=data_pegawai.nik INNER JOIN data_jabatan ON data_jabatan.nama_jabatan = data_pegawai.jabatan WHERE data_kehadiran.nik='$nik' ORDER BY data_kehadiran.bulan DESC")->result();
+    }
 }
 
