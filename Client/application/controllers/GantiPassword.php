638121740d94359b1aa28e44f3f5cf25ffd0319c
Pratama Budi <pratamabudi455@gmail.com>
2023-01-25 12:54:44 +0700

    prataman : membuat function _rules di controller ganti password untuk mengecek apakah password yang di input dan repassword sama atau tidak diisi

--- Client/application/controllers/GantiPassword.php
+++ Client/application/controllers/GantiPassword.php
@@ -10,6 +10,11 @@ class GantiPassword extends CI_Controller{
         $this->load->view('ganti_password', $data);
         $this->load->view('templates_admin/footer');
     }
+
+    public function _rules(){
+        $this->form_validation->set_rules('password_baru', 'Password Baru', 'required|matches[ulangi_password]');
+        $this->form_validation->set_rules('ulangi_password', 'Ulangi Password', 'required');
+    }
 }
 
 ?>
