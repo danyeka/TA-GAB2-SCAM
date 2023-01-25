ff30401d401faa90df75ca3b8b7f1581455388bd
Dany Eka <ekaadany12@gmail.com>
2023-01-25 12:02:22 +0700

    dany : membuat function index di controller user, untuk validasi user dan memasang session

--- Client/application/controllers/Welcome.php
+++ Client/application/controllers/Welcome.php
@@ -5,7 +5,43 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->_rules();
+		if ($this->form_validation->run() == FALSE){
+			$data['title'] = "Form - Login";
+            $this->load->view('templates_admin/header',$data);
+			$this->load->view('welcome_message',$data);
+        } else {
+			$send = array('username' => $this->input->post('username'), 'password' => $this->input->post('password'));
+			$check = json_decode($this->client->simple_post(API_VALIDASI, $send));
+			if ($check->login == 1){
+				$this->session->set_userdata('hak_akses', $check->hak_akses);
+				$this->session->set_userdata('nama_pegawai', $check->nama_pegawai);
+				$this->session->set_userdata('photo', $check->photo);
+				$this->session->set_userdata('id_pegawai', $check->id_pegawai);
+				$this->session->set_userdata('nik', $check->nik);
+				switch($check->hak_akses){
+					case 1:
+						redirect("/admin/Dashboard");
+						break;
+					case 2:
+						redirect("/pegawai/Dashboard");
+						break;
+
+					default:
+						break;
+				}
+			} else {
+				$this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Username atau password Salah</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+				redirect("/");
+			}
+		}
+        
+	}
 
 	public function _rules(){
         $this->form_validation->set_rules('username', 'Username', 'required');
