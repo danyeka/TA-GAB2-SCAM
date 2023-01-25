82f8c5fd09e7c7fc544af7752a536989a01cf3a6
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:00:10 +0700

    rifan : membuat function get_data di model potongan gaji

--- Server/application/models/Mpotongan_gaji.php
+++ Server/application/models/Mpotongan_gaji.php
@@ -2,5 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mpotongan_gaji extends CI_Model {
+
+    function get_data($id)
+    {
+        $this->db->select("*");
+        $this->db->from("potongan_gaji");
+        if(empty($id)) $this->db->order_by("potongan", "ASC");
+        else $this->db->where("id = '$id'");
+        return $this->db->get()->result();
+    }
 }
 
