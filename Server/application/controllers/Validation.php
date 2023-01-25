f9608e7dd492d4fb3c431d389cc4439ea029480f
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:17:15 +0700

    rifan : membuat __construct untuk mengambil model gaji dan pegawai di controller validation

--- Server/application/controllers/Validation.php
+++ Server/application/controllers/Validation.php
@@ -5,4 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class Validation extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Mpegawai","model",TRUE);
+        $this->load->model("Mgaji","gaji",TRUE);
+    }
 }
