75930fb7cac8166f8b723c5f59f3c8700f824aa2
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:16:58 +0700

    rifan : membuat __construct untuk mengambil model pegawai di controller data update password

--- Server/application/controllers/UpdatePassword.php
+++ Server/application/controllers/UpdatePassword.php
@@ -5,4 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class UpdatePassword extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Mpegawai","model",TRUE);
+    }
 }
