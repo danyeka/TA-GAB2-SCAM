fbea7df41eeb2ad08d13f75540f52b80d067fabc
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:26:47 +0700

    rifan : membuat function service post controller update password untuk mengupdate password semua pegawai atau admin berdasarkan id

--- Server/application/controllers/UpdatePassword.php
+++ Server/application/controllers/UpdatePassword.php
@@ -11,4 +11,16 @@ class UpdatePassword extends Server {
         parent::__construct();
         $this->load->model("Mpegawai","model",TRUE);
     }
+
+    function service_post()
+    {
+        $data = array(
+            "password" => $this->post("password_baru")
+        );
+    
+
+        $hasil = $this->model->update_data($data, $this->post("id_pegawai"));
+        if($hasil == 0 ) $this->response(array("pesan" => "true"),200);
+        else $this->response(array("pesan" => "false"),200);
+    }
 }
