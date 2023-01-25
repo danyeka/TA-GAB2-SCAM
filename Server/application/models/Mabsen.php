6197abd27ca1c01e6cdcff130d4bf5c860c5b0a1
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 10:55:48 +0700

    rifan : membuat function get_data di model absen

--- Server/application/models/Mabsen.php
+++ Server/application/models/Mabsen.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Mabsen extends CI_Model {
 
+    function get_data($bulantahun)
+    {
+        return $this->db->query("SELECT data_kehadiran.*, data_pegawai.nama_pegawai, data_pegawai.jenis_kelamin, data_pegawai.jabatan FROM data_kehadiran INNER JOIN data_pegawai ON data_kehadiran.nik = data_pegawai.nik INNER JOIN data_jabatan ON data_pegawai.jabatan = data_jabatan.nama_jabatan WHERE data_kehadiran.bulan = '$bulantahun' ORDER BY data_pegawai.nama_pegawai ASC")->result();
+    }
     
     
 }
