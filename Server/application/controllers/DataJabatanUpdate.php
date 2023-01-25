3f98eb072f33514d0abff6703a2144b99e73b1f5
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:21:57 +0700

    rifan : membuat function service post controller data jabatan update untuk mengupdate data jabatan

--- Server/application/controllers/DataJabatanUpdate.php
+++ Server/application/controllers/DataJabatanUpdate.php
@@ -20,4 +20,22 @@ class DataJabatanUpdate extends Server {
             $this->response(array("pesan" => "false"),200);
         }
     }
+
+    function service_post()
+    {
+        $data = array(
+            'nama_jabatan' => $this->input->post('nama_jabatan'),
+            'gaji_pokok' => $this->input->post('gaji_pokok'),
+            'tj_transport' => $this->input->post('tj_transport'),
+            'uang_makan' => $this->input->post('uang_makan')
+        );
+
+        $hasil = $this->model->update_data($this->input->post('id_jabatan'), $data);
+        
+        if($hasil == 0) {
+            $this->response(array("pesan" => "true"),200);
+        } else {
+            $this->response(array("pesan" => "false"),200);
+        }
+    }
 }
