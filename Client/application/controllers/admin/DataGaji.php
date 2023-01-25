d6c6d7296825d433d6bd07ffcec7e9df2fe6ace5
Pratama Budi <pratamabudi455@gmail.com>
2023-01-25 12:47:13 +0700

    pratama : membuat function index di controller data gaji admin untuk mengambil semua data gaji karyawan berdasarkan bulan dan tahun

--- Client/application/controllers/admin/DataGaji.php
+++ Client/application/controllers/admin/DataGaji.php
@@ -16,6 +16,27 @@ class DataGaji extends CI_Controller{
         }
     }
     
+    public function index()
+    {
+        $bulan = date('m');
+        $tahun = date('Y');
+        $bulantaun = $bulan . $tahun;
+
+        if(!empty($this->input->get('bulan')) && !empty($this->input->get('bulan'))) $send = array('bulantahun' => $this->input->get('bulan') . $this->input->get('tahun'));
+        else $send = array('bulantahun' => $bulantaun);
+
+        $potonganGaji = json_decode($this->client->simple_get(API_POTONGAN_GAJI, $send));
+        $data['potongan_gaji'] = $potonganGaji->potongan_gaji[0]->jml_potongan;
+
+        $data['title'] = "Data Gaji Pegawai";
+        $gaji = json_decode($this->client->simple_get(API_DATA_GAJI, $send));
+        $data['gaji'] = $gaji->gaji;
+
+        $this->load->view('templates_admin/header',$data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/data_gaji',$data);
+        $this->load->view('templates_admin/footer');
+    }
 }
 
 ?>
