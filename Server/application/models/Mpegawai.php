cb6d540c6f643646b183ba3b13641a95383d6089
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 10:59:02 +0700

    rifan : membuat function get_data_admin di model pegawai

--- Server/application/models/Mpegawai.php
+++ Server/application/models/Mpegawai.php
@@ -11,5 +11,14 @@ class Mpegawai extends CI_Model {
         else $this->db->where("id_pegawai = '$id'");
         return $this->db->get()->result();
     }
+
+    function get_data_admin()
+    {
+        $this->db->select("*");
+        $this->db->from("data_pegawai");
+        $this->db->where("jabatan = 'Admin'");
+        $this->db->order_by("nama_pegawai", "ASC");
+        return $this->db->get()->result();
+    }
 }
 
