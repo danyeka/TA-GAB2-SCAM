41d0d5db8ebe161da09f38b6a6f4f82923235187
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:23:02 +0700

    rifan : membuat function service post controller data pegawai untuk menambahkan data pegawai

--- Server/application/controllers/DataPegawai.php
+++ Server/application/controllers/DataPegawai.php
@@ -24,4 +24,22 @@ class DataPegawai extends Server {
             $this->response(array("pegawai" => $this->model->get_data($this->input->get('id'))),200);
         }
     }
+
+    function service_post()
+    {
+        $data = array(
+            "nik" => $this->post("nik"),
+            "nama_pegawai" => $this->post("nama_pegawai"),
+            "jenis_kelamin" => $this->post("jenis_kelamin"),
+            "jabatan" => $this->post("jabatan"),
+            "tanggal_masuk" => $this->post("tanggal_masuk"),
+            "status" => $this->post("status"),
+            "hak_akses" => $this->post("hak_akses"),
+            "photo" => $this->post("photo")
+        );
+
+        $hasil = $this->model->save_data($data);
+        if($hasil == 0 ) $this->response(array("pesan" => "true"),200);
+        else $this->response(array("pesan" => "false"),200);
+    }
 }
