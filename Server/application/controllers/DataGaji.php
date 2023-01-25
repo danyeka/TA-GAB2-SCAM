503886f38eaf11ca0d8f11232eb469f77204472b
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:14:21 +0700

    rifan : membuat __construct untuk mengambil model gaji di controller data gaji

--- Server/application/controllers/DataGaji.php
+++ Server/application/controllers/DataGaji.php
@@ -4,4 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class DataGaji extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Mgaji","model",TRUE);
+    }
 }
