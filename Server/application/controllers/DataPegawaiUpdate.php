9ced517690fe73eb1601a01f855358d0f74164a5
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:15:58 +0700

    rifan : membuat __construct untuk mengambil model pegawai di controller data pegawai update

--- Server/application/controllers/DataPegawaiUpdate.php
+++ Server/application/controllers/DataPegawaiUpdate.php
@@ -5,4 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class DataPegawaiUpdate extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("Mpegawai","model",TRUE);
+    }
+
 }
