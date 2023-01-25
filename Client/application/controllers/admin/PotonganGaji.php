714d4ff9dace4b6a072466d07dfa2f86b133021f
Pratama Budi <pratamabudi455@gmail.com>
2023-01-25 13:05:30 +0700

    dany : membuat function tambah data aksi digunakan untuk memvalidasi tambah data dan menyimpan ke database menggunakan api

--- Client/application/controllers/admin/PotonganGaji.php
+++ Client/application/controllers/admin/PotonganGaji.php
@@ -39,6 +39,42 @@ class PotonganGaji extends CI_Controller{
         $this->load->view('admin/tambah_potongan_gaji',$data);
         $this->load->view('templates_admin/footer');
     }
+
+    public function tambah_data_aksi()
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE){
+            $this->tambah_data();
+        } else {
+            $jumlah_potongan = $this->input->post('jumlah_potongan');
+            $jenis_potongan = $this->input->post('jenis_potongan');
+
+            $data = array(
+                'jumlah_potongan'   => $this->input->post('jumlah_potongan'),
+                'jenis_potongan'    => $this->input->post('jenis_potongan')
+            );
+
+            $response = json_decode($this->client->simple_post(API_POTONGAN_GAJI, $data));
+            if ($response->pesan){
+                $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Data berhasil ditambahkan</strong> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            } else {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Data gagal ditambahkan</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            }
+
+            $this->index();
+        }
+    }
 }
 
 ?>
