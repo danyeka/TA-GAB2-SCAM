6e6d06d2588b59a82182ecdbac7d999a1a11be8c
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:20:37 +0700

    rifan : membuat function service post controller data jabatan untuk menyimpan data jabatan

--- Server/application/controllers/DataJabatan.php
+++ Server/application/controllers/DataJabatan.php
@@ -22,4 +22,22 @@ class DataJabatan extends Server {
             $this->response(array("jabatan" => $this->model->get_data($this->input->get('id'))),200);
         }
     }
+
+    function service_post()
+    {
+        $data = array(
+            'nama_jabatan' => $this->input->post('nama_jabatan'),
+            'gaji_pokok' => $this->input->post('gaji_pokok'),
+            'tj_transport' => $this->input->post('tj_transport'),
+            'uang_makan' => $this->input->post('uang_makan')
+        );
+
+        $hasil = $this->model->save_data($data);
+        
+        if($hasil == 0) {
+            $this->response(array("pesan" => "true"),200);
+        } else {
+            $this->response(array("pesan" => "false"),200);
+        }
+    }
 }
