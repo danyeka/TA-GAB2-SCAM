7f8e1b7bb7e0d486c616d700a4f005921d19853f
Rifan Abiyoga <rifanabiyoga391@gmail.com>
2023-01-25 11:21:06 +0700

    rifan : membuat function service get controller data jabatan update untuk mengapus data jabatan

--- Server/application/controllers/DataJabatanUpdate.php
+++ Server/application/controllers/DataJabatanUpdate.php
@@ -11,4 +11,13 @@ class DataJabatanUpdate extends Server {
         parent::__construct();
         $this->load->model("Mjabatan", "model", TRUE);
     }
+
+    function service_get()
+    {
+        if($this->model->delete_data($this->input->get('id_jabatan')) == 0) {
+            $this->response(array("pesan" => "true"),200);
+        } else {
+            $this->response(array("pesan" => "false"),200);
+        }
+    }
 }
