069e07e61fc5732cf7e3cf0666181958442c3a78
Dany Eka <ekaadany12@gmail.com>
2023-01-27 00:36:58 +0700

    dany : update dashboard

--- Client/application/views/admin/dashboard.php
+++ Client/application/views/admin/dashboard.php
@@ -18,7 +18,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Data Pegawai</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">40,000</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $pegawai ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-users fa-2x text-gray-300"></i>
@@ -37,7 +37,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                 Data Admin</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">215,000</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $admin ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-user-cog fa-2x text-gray-300"></i>
@@ -57,7 +57,7 @@
                             </div>
                             <div class="row no-gutters align-items-center">
                                 <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50</div>
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $jabatan ?></div>
                                 </div>
                             </div>
                         </div>
@@ -78,7 +78,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
                                 Data Kehadiran</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $kehadiran ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-comments fa-2x text-gray-300"></i>
