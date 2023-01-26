23815aa19db814ce01d648f6e1f3518e6cb1e787
rifanabiyoga391 <rifanabiyoga391@gmail.com>
2023-01-27 04:02:19 +0700

    master: update database

--- db_penggajian.sql
+++ db_penggajian.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2022 at 04:16 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Jan 26, 2023 at 10:01 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -38,6 +38,14 @@ CREATE TABLE `data_jabatan` (
   `uang_makan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `data_jabatan`
+--
+
+INSERT INTO `data_jabatan` (`id_jabatan`, `nama_jabatan`, `gaji_pokok`, `tj_transport`, `uang_makan`) VALUES
+(2, 'Staff Finance', '150000', '100000', '15000'),
+(7, 'Staff Admin', '150000000', '15000000', '1500000');
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +64,13 @@ CREATE TABLE `data_kehadiran` (
   `alpha` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `data_kehadiran`
+--
+
+INSERT INTO `data_kehadiran` (`id_kehadiran`, `bulan`, `nik`, `nama_karyawan`, `jenis_kelamin`, `nama_jabatan`, `hadir`, `sakit`, `alpha`) VALUES
+(1, '022022', '1234', 'Alzi', 'Laki-Laki', 'Staff Admin', 20, 1, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -66,19 +81,63 @@ CREATE TABLE `data_pegawai` (
   `id_pegawai` int(11) NOT NULL,
   `nik` varchar(50) NOT NULL,
   `nama_pegawai` varchar(225) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL,
   `jenis_kelamin` varchar(20) NOT NULL,
   `jabatan` varchar(50) NOT NULL,
   `tanggal_masuk` date NOT NULL,
   `status` varchar(10) NOT NULL,
-  `photo` varchar(225) NOT NULL
+  `photo` varchar(225) NOT NULL,
+  `hak_akses` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `data_pegawai`
 --
 
-INSERT INTO `data_pegawai` (`id_pegawai`, `nik`, `nama_pegawai`, `jenis_kelamin`, `jabatan`, `tanggal_masuk`, `status`, `photo`) VALUES
-(1, '1234', 'alzi', 'laki', 'karyaawan tetap', '2022-12-13', 'aktif', '-');
+INSERT INTO `data_pegawai` (`id_pegawai`, `nik`, `nama_pegawai`, `username`, `password`, `jenis_kelamin`, `jabatan`, `tanggal_masuk`, `status`, `photo`, `hak_akses`) VALUES
+(1, '1234', 'Alzi', 'alzi', '123', 'Laki-Laki', 'Staff Admin', '2022-12-13', 'aktif', 'avatar.png', 2),
+(2, '123123123', 'Admin', 'admin', 'admin', 'Laki-Laki', 'Admin', '2023-01-04', 'aktif', 'Group_1_(3).png', 1),
+(4, '9827349834', 'apa', 'apa', 'apa', 'Laki-Laki', 'Staff Finance', '2023-01-18', 'Pegawai Te', 'Group_1_(2).png', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hak_akses`
+--
+
+CREATE TABLE `hak_akses` (
+  `id` int(11) NOT NULL,
+  `keterangan` varchar(50) NOT NULL,
+  `hak_akses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hak_akses`
+--
+
+INSERT INTO `hak_akses` (`id`, `keterangan`, `hak_akses`) VALUES
+(1, 'admin', 1),
+(2, 'karyawan', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `potongan_gaji`
+--
+
+CREATE TABLE `potongan_gaji` (
+  `id` int(11) NOT NULL,
+  `potongan` varchar(120) NOT NULL,
+  `jml_potongan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `potongan_gaji`
+--
+
+INSERT INTO `potongan_gaji` (`id`, `potongan`, `jml_potongan`) VALUES
+(4, 'alpha', 15000);
 
 --
 -- Indexes for dumped tables
@@ -103,6 +162,18 @@ ALTER TABLE `data_pegawai`
   ADD PRIMARY KEY (`id_pegawai`);
 
 --
+-- Indexes for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `potongan_gaji`
+--
+ALTER TABLE `potongan_gaji`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -110,19 +181,31 @@ ALTER TABLE `data_pegawai`
 -- AUTO_INCREMENT for table `data_jabatan`
 --
 ALTER TABLE `data_jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `data_kehadiran`
 --
 ALTER TABLE `data_kehadiran`
-  MODIFY `id_kehadiran` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kehadiran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `data_pegawai`
 --
 ALTER TABLE `data_pegawai`
-  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `potongan_gaji`
+--
+ALTER TABLE `potongan_gaji`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
